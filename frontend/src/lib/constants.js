1912868e5a5dd2953516ce7f79b3438ab3a4ee3c
Vamsi kaki <vamsi@myaibo.in>
2026-05-21 21:29:23 +0530

    calender

--- frontend/src/lib/constants.js
+++ frontend/src/lib/constants.js
@@ -1,5 +1,5 @@
 export const BOOKING_URL =
-  "https://calendar.google.com/calendar/u/0/appointments/schedules/AcZssZ1RTsHL7_2Q0w8M2J7ul--JslQuYdb9C9Bfd5yLu0i5VodhSqHB0i9gkvUCTKQ476GMWqETg3QB";
+  "https://calendar.app.google/fhN7TSzLypCQT4tQ8";
 
 /**
  * API base, always ending in /api. Resolution order:
